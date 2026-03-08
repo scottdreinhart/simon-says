4ee7443b5be139aca14cf9c566a9dd0a088aed6e
Scott Reinhart <scottdreinhart@users.noreply.github.com>
2026-03-08 14:28:56 -0400

    initial scaffold: React + Vite + TypeScript + Electron + Capacitor

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Simon Says</h1>
+      <p>Repeat a growing sequence of colors/sounds; memory challenge</p>
+    </div>
+  )
+}
